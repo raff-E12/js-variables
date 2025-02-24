fec0879e0048d566b1b328519d064ba350aad319
Raff_12 <raffelecoppola381@gmail.com>
2025-02-24 17:48:06 +0100

    feat: Modifica di variabile

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,8 +1,8 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-let variabile = 'Nome';
-console.log(variabile);
+let Nome = 'Raffaele';
+console.log(Nome);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
